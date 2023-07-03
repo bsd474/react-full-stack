2c8972a4b33cd77cf643ce906b34a11b80b12270
Bader Aldossary <baldossary@gmail.com>
2023-07-03 01:10:26 -0700

    Pages and Children Props

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header />
-    <div className="max-w-6xl mx-auto">
-      <App />
-    </div>
+    <App />
   </React.StrictMode>
 );
